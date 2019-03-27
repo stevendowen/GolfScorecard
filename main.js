7fb1ed6d1f1251d2442b56154c2b0af6f64d2a51
Steven Owen <stevenowen83@gmail.com>
2019-03-27 14:11:04 -0600

    appended cards for courses with images and names, added MDL and created css for the layout and cards

--- main.js
+++ main.js
@@ -1,12 +1,27 @@
-(function loadCourses() {
+
+
+loadCourses();
+
+function loadCourses() {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
+
             let courses = JSON.parse(this.responseText);
             console.log(courses);
-
+            for(let i = 0; i < courses.courses.length; i++){
+                let course = courses.courses[i];
+                let img = course.image;
+                $('.courses').append(`<div class="course mdl-card-square mdl-shadow--16dp">
+                <img src="${img}">
+                <div class="cardbottom mdl-card__actions">
+                <h2 class="mdl-card__title-text">${course.name}</h2>
+                <button class="mdl-button mdl-js-button mdl-js-ripple-effect">Button</button>
+                </div>
+                </div>`);
+            }
         }
     };
     xhttp.open("GET", "https://golf-courses-api.herokuapp.com/courses", true);
     xhttp.send();
-})();
+}
