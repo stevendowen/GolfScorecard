1c4e79c7fb4ded7895b9df5df43f7b383c6a83de
Steven Owen <stevenowen83@gmail.com>
2019-04-01 15:58:18 -0600

    added total yards and par with css

--- main.js
+++ main.js
@@ -2,6 +2,11 @@
 
 let selectedcourse;
 let partotal;
+let yardtotal;
+let totalpar = [];
+let totalyards = [];
+let allpar;
+let allyards;
 const add = (a,b) => a + b;
 
 function showCourses(){
@@ -50,11 +55,10 @@ function loadCourse(courseid) {
             console.log(selectedcourse);
             $('.scorecard').append(`<div class="titlebox"><div class="title">Hole</div>
             <div class="title">Par</div>
+            <div class="title">Yards</div>
+            <div class="title">Handicap</div>
             </div>`);
             displayPlayers();
-            $('.coursemain').append(`<div class="titlebottom"><div class="title">Handicap</div>
-            <div class="title">Yards</div>
-            </div>`);
             loadCourseInfo(courseid);
 
         }
@@ -82,54 +86,83 @@ function loadCourseInfo(){
 function displayFrontNine(){
     let holes = selectedcourse.data.holes;
     let pars = [];
+    let yards = [];
     for(let h = 0; h < 9; h++){
         let hole = holes[h].hole;
         let par = holes[h].teeBoxes[0].par;
+        let yard = holes[h].teeBoxes[0].yards;
+        let handicap = holes[h].teeBoxes[0].hcp;
         $('.scorecard').append(`<div><div class="hole">${hole}</div>
                 <div class="par">${par}</div>
+                <div class="par">${yard}</div>
+                <div class="par">${handicap}</div>
                 </div>`);
         $('.coursemain').fadeIn(600);
         pars.push(par);
+        yards.push(yard);
         partotal = pars.reduce(add);
+        yardtotal = yards.reduce(add);
     }
     $('.scorecard').append(`<div class="totalbox">
     <div class="total">Out</div>
     <div class="total">${partotal}</div>
+    <div class="total">${yardtotal}</div>
     </div>`);
+    totalpar.push(partotal);
+    totalyards.push(yardtotal);
     displayBackNine();
 }
 
 function displayBackNine(){
     let holes = selectedcourse.data.holes;
     let pars = [];
+    let yards = [];
     for(let h = 9; h < holes.length; h++){
         let hole = holes[h].hole;
         let par = holes[h].teeBoxes[0].par;
+        let yard = holes[h].teeBoxes[0].yards;
+        let handicap = holes[h].teeBoxes[0].hcp;
         $('.scorecard').append(`<div><div class="hole">${hole}</div>
                 <div class="par">${par}</div>
+                <div class="par">${yard}</div>
+                <div class="par">${handicap}</div>
                 </div>`);
         $('.coursemain').fadeIn(600);
         pars.push(par);
+        yards.push(yard);
         partotal = pars.reduce(add);
+        yardtotal = yards.reduce(add);
     }
     $('.scorecard').append(`<div class="totalbox">
     <div class="total">In</div>
     <div class="total">${partotal}</div>
+    <div class="total">${yardtotal}</div>
     </div>`);
-    $('.scorecard').append(`<div class="totalbox">Total</div>`);
-    displayHcpFrontNine();
+    totalpar.push(partotal);
+    totalyards.push(yardtotal);
+    allpar = totalpar.reduce(add);
+    allyards = totalyards.reduce(add);
+    $('.scorecard').append(`<div class="totalbox">
+    <div class="total">Total</div>
+    <div class="total">${allpar}</div>
+    <div class="total">${allyards}</div>  
+    </div>`);
 }
 
 function displayPlayers(){
+    $('.playerbox').html("");
     $('.coursemain').append(`<div class="playerbox">
     <div class="namebox">
     <input class="input" type="text" placeholder="Add Player" onkeyup="addPlayer(this.value, event)">
     </div>
-    <div>Players Scores</div>
+    <div class="scores"></div>
     </div>`);
     for(let p = 0; p < golfplayers.playerCollection.length; p++){
         let players = golfplayers.playerCollection[p].name;
-        $('.namebox').append(`<div>${players}</div>`);
+        $('.namebox').append(`<div class="player">${players}</div>`);
+        for(let h = 0; h < 9; h++){
+            $('.scores').append(`<div class="holescore">${p}${h}</div>`);
+        }
         if(golfplayers.playerCollection.length === 4){
             $('.input').hide();
         }
@@ -142,18 +175,9 @@ function addPlayer(val, event){
         case 13:
             let id = golfplayers.playerCollection.length + 1;
             golfplayers.addPlayer(id, val);
-            $('.playerbox').html("");
             displayPlayers();
             $('.input').val('');
             $('.input').focus();
             break;
-    }
-}
-
-function displayHcpFrontNine(){
-    let holes = selectedcourse.data.holes;
-    for(let h = 0; h < 9; h++) {
-        let hcp = holes[h].teeBoxes[0].hcp;
-        let yards = holes[h].teeBoxes[0].yards;
     }
 }
